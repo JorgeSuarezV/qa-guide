e4d6d3274b8f4eb057d1a111a648f3acb33f097d
Jorge <jorge.sv.primero@gmail.com>
2024-08-07 22:02:13 -0300

    done

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient({
+  datasourceUrl: 'postgresql://prisma:prisma@localhost:5433/tests',
+});
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
@@ -21,4 +25,23 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('create n users', async () => {
+    const users = createNUsers(10);
+    await prisma.user.createMany({ data: users });
+    const user4 = await prisma.user.findFirst({ where: { name: 'user4' } });
+    expect(user4.name).toEqual('user4');
+  });
 });
+
+function createNUsers(n: number) {
+  const users = [];
+  for (let i = 0; i < n; i++) {
+    users.push({
+      name: `user${i}`,
+      email: `user${i}@mail.com`,
+      password: `password${i}`,
+    });
+  }
+  return users;
+}
