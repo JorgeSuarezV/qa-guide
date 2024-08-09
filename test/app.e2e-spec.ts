fd59c2e8550abcca6c5b73ec7b61d894b92c2edb
Jorge <jorge.sv.primero@gmail.com>
2024-08-08 21:13:24 -0300

    Hello

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -23,7 +23,7 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Hello');
   });
 
   it('create n users', async () => {
